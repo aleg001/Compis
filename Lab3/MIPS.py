fd2aa7dd7b57183e76e81efe43ac313d33ed78a2
Ale Gomez <gom20347@uvg.edu.gt>
2023-11-05 23:23:38 -0600

    added init version of MIPS parser

--- Lab3/MIPS.py
+++ Lab3/MIPS.py
@@ -18,6 +18,18 @@ def parse_icr(icr):
             mips_output.append(pushing(words[1]))
         elif "CALLING" in line and len(words) > 4:
             mips_output.append(calling_function(words[1], int(words[3])))
+        
+        elif "out_string" in line:
+            string = line.split('"')[1]  
+            mips_output.extend(handle_out_string(string))
+        elif "out_int" in line:
+            integer = int(words[-1])
+            mips_output.extend(handle_out_int(integer))
+    
+        elif "FUNCTION" in line:
+            mips_output.append(".globl {}".format(words[1]))  
+            mips_output.append("{}:".format(words[1]))
+            
         elif "=" in line and ("+" in line or "/" in line):
             result, _, operand1, op, operand2 = words[:5]
             mips_output.extend(arithmetic_operation(result, operand1, op, operand2))
@@ -25,8 +37,9 @@ def parse_icr(icr):
     return mips_output
 
 
+
 def declare_class(name):
-    return f"\n# INIT clase {name}\n"
+    return f"\n# INIT class {name}\n"
 
 
 def init_block(label):
@@ -41,7 +54,6 @@ def pushing(value):
     if isinstance(value, str) and value.startswith("t"):
         return f"lw $sp, {value}\naddiu $sp, $sp, -4\n"
     else:
-
         return f"li $t0, {value}\nlw $sp, $t0\naddiu $sp, $sp, -4\n"
 
 
@@ -58,3 +70,34 @@ def arithmetic_operation(result, operand1, op, operand2):
         mips_instructions.append(f"div {operand1}, {operand2} # División\n")
         mips_instructions.append(f"mflo {result}\n")
     return mips_instructions
+
+
+def system_call(service_code):
+    return [
+        'li $v0, {}'.format(service_code),  
+        'syscall'                           
+    ]
+
+
+def handle_out_string(string):
+    mips_instructions = []
+    string_label = "str{}".format(len(string))  
+ 
+    mips_instructions.append('.data')
+    mips_instructions.append('{}: .asciiz {}'.format(string_label, string))
+    mips_instructions.append('.text')
+
+    mips_instructions.append('la $a0, {}'.format(string_label))
+
+    mips_instructions.extend(system_call(4))
+    return mips_instructions
+
+
+def handle_out_int(integer):
+    mips_instructions = [
+        'li $a0, {}'.format(integer),  
+    ]
+ 
+    mips_instructions.extend(system_call(1))
+    return mips_instructions
+
